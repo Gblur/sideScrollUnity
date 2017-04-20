e93ec8d8fea6724fff0f10f3ae61974efd2554d8
Marian Gaebler <marian.gaebler@gmail.com>
2017-04-20 23:53:37 +0200

    Refactor from game object to enemy

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject enemy;
-	private GameObject[] enemies;
+	public Enemy enemy;
+	private Enemy[] enemies;
 	// Use this for initialization
 	void Start () {
-		enemies = new GameObject[3];
+		enemies = new Enemy[3];
 		for (int i = 0; i < enemies.Length; i++) {
-			GameObject clone = Instantiate (
+			Enemy clone = Instantiate (
 				enemy, 
 				new Vector2(10,i * 8), 
 				transform.rotation
@@ -23,8 +23,8 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		// @todo kill objects if they're out of the canvas
-		foreach (GameObject enemy in enemies) {
-			enemy.transform.Translate (Vector2.left * Time.deltaTime * 5);
+		foreach (Enemy enemy in enemies) {
+			enemy.move (Vector2.left * Time.deltaTime * 5);
 		}
 	}
 }
