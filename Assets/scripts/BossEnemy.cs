dbc7962626940c6503a28d852673c3110aa1f431
Marian Gaebler <marian.gaebler@gmail.com>
2017-04-20 23:51:24 +0200

    Create basic behaviour of enemy state.

---
+++ Assets/scripts/BossEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BossEnemy : Enemy {
+
+	public Image healthBar;
+
+	public void gotHit() {
+		healthBar.fillAmount -= 0.2f;
+	}
+}
---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Enemy : MonoBehaviour {
+
+//	public string name = "Foo";
+	public int health = 100;
+	public int attackPower;
+	public int attackRate;
+
+	private string state = "uninitialized";
+
+	const string STATE_ALIVE = "alive";
+	const string STATE_DEAD = "dead";
+
+
+	public void Start(){
+		this.name = name;
+		this.setState (STATE_ALIVE);
+
+	}
+
+	public void gotHit(int damage) {
+		this.health -= damage;
+		Debug.Log (health);
+		if (this.health < 0) {
+			Destroy (this.gameObject);
+			this.setState (STATE_DEAD);		
+		}		
+	}
+
+	private void setState(string state) {
+		this.state = state;
+		switch (state) {
+		case STATE_DEAD:
+			
+
+			break;
+		}
+	}
+
+	void OnParticleCollision(GameObject other) {
+		Debug.Log ("Treffer");
+		this.gotHit (50);
+	}
+
+	public void move(Vector2 coords) {
+		if (this.state != STATE_DEAD) {
+			this.gameObject.transform.Translate (coords);
+		}
+	}
+}
