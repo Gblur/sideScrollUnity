b0b316edae938b9ff85f2b40a657cf18bb98a8e4
Florian <floriangaebler87@gmail.com>
2017-05-03 20:41:51 +0200

    Movement Parameter
    now enemy will move by curve presets

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    
+    public AnimationCurve xCurve;
 	public Enemy enemy;
 	private Enemy[] enemies;
-	// Use this for initialization
-	void Start () {
+    public float Yrange = .5f;
+    public int EnemySpeed = 5;
+    // Use this for initialization
+    
+        
+    
+
+    void Start () {
+
+
 		enemies = new Enemy[3];
 		for (int i = 0; i < enemies.Length; i++) {
 			Enemy clone = Instantiate (
 				enemy, 
-				new Vector2(10,i * 8), 
+				new Vector2(27,i * 8), 
 				transform.rotation
 			);
 			enemies [i] = clone;
@@ -22,9 +32,14 @@ public class GameManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		// @todo kill objects if they're out of the canvas
-		foreach (Enemy enemy in enemies) {
-			enemy.move (Vector2.left * Time.deltaTime * 5);
-		}
+
+        float YPos = xCurve.Evaluate(Time.time) * Yrange;
+       
+
+        // @todo kill objects if they're out of the canvas
+        foreach (Enemy enemy in enemies) {
+            enemy.move(new Vector2(-1 * Time.deltaTime * EnemySpeed , YPos));
+                
+                }
 	}
 }
