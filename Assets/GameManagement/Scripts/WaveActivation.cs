758022568391eb30cd3c48af5838c4dbe488663b
Florian <floriangaebler87@gmail.com>
2017-05-22 17:58:28 +0200

    Scripts Added
    WaveTimer Gets value from Time.time node and write it on Canvas TextObject
    WaveActivation let a ParticleSystem emit Particles after a predeterminded time.

---
+++ Assets/GameManagement/Scripts/WaveActivation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveActivation : MonoBehaviour {
+
+    
+    
+    public int startWave;
+    private ParticleSystem particles;
+	// Use this for initialization
+	void Start () {
+        //initialize EmissionModule of ParticleSystem
+        particles = GetComponent<ParticleSystem>();
+        var em = particles.emission;
+        em.enabled = false;
+        
+
+
+    }
+
+    public void EmissionTime()
+    {
+
+      
+
+        //Emit Particles on Time
+        particles = GetComponent<ParticleSystem>();
+        var em = particles.emission;
+        
+
+        if (Time.time >= startWave)
+        {
+            em.enabled = true;
+        }
+        
+                
+
+
+        } 
+        
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+        EmissionTime();
+        //Debug.Log(Time.time);
+        
+    }
+}
---
+++ Assets/GameManagement/Scripts/WaveTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveTimer : MonoBehaviour {
+
+    private Text countText;
+    private int Count;
+
+	// Use this for initialization
+	void Start () {
+       countText = GetComponent<Text>();
+	}
+    
+
+    public void Waves() {
+
+        
+        countText.text = Time.time.ToString("0");
+        //Debug.Log(Time.time);
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Waves();
+
+    }
+}
