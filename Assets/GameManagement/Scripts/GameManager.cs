7ce86fb22b6a5b77a52b946a07cd49a875073c40
Florian <floriangaebler87@gmail.com>
2017-08-14 19:58:35 +0200

    cleanup

--- Assets/GameManagement/Scripts/GameManager.cs
+++ Assets/GameManagement/Scripts/GameManager.cs
@@ -2,45 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-    
+
     public AnimationCurve xCurve;
-	public Enemy enemy;
+    public Enemy enemy;
+    public Vector2 spawnValues;
     public Enemy otherEnemy;
-	private Enemy[] enemies;
+    private Enemy[] enemies;
     public float Yrange = .5f;
     public int EnemySpeed = 5;
+    public float spawnWait = 5.0f;
+    public float startWait;
+    public float WaveWait;
+    public float space = -47;
     // Use this for initialization
-    
+
+
+
+
+    void Start()
+    {
         
-    
+        StartCoroutine(SpawnWaves());
 
-    void Start () {
+    }
 
-
-		enemies = new Enemy[3];
-		for (int i = 0; i < enemies.Length; i++) {
-			Enemy clone = Instantiate (
-				enemy, 
-				new Vector2(27,i * 8), 
-				transform.rotation
-			);
-			enemies [i] = clone;
-		}
-	}
+    public IEnumerator SpawnWaves()
+    {
 
 
-	// Update is called once per frame
-	void Update () {
+        yield return new WaitForSeconds(startWait);
+        while(Time.time < 20)
+        {
+            enemies = new Enemy[25];
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Vector2 YSpace = new Vector2(spawnValues.x, Random.Range(-spawnValues.y, spawnValues.y));
+                Enemy clone = Instantiate(
+                    enemy,
+                    YSpace,
+                    transform.rotation
+                );
+
+                enemies[i] = clone;
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(WaveWait);
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
 
         float YPos = xCurve.Evaluate(Time.time) * Yrange;
-       
 
-        // @todo kill objects if they're out of the canvas
-        foreach (Enemy enemy in enemies) {
-            enemy.move(new Vector2(-1 * Time.deltaTime * EnemySpeed , YPos));
-                
-                }
-	}
+
+        foreach (Enemy enemy in enemies)
+        {
+
+            enemy.move(new Vector2(-1 * Time.deltaTime * EnemySpeed, YPos));
+        }
+    }
 }
+
+        
